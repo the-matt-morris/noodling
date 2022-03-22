70e9eb06bfe9a12c0502d8aca9ba4ddc0ca227f8
root <matt.morris@rsginc.com>
2022-03-22 21:42:14 +0000

    add docstrings

--- project/utils.py
+++ project/utils.py
@@ -2,5 +2,14 @@ import math
 
 
 def pythagorean(x: float, y: float) -> float:
+    """Pythagorean theorem (c**2 = sqrt(x**2 + y**2))
+
+    Args:
+        x (float): length of one side
+        y (float): length of adjacent side
+
+    Returns:
+        float: length of hypotenuse
+    """
     return math.sqrt(x**2 + y**2)
     
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,15 @@
-import inspect
 import pytest
-from typing import Any, Callable, List
 
 from project.utils import pythagorean
 
 
-def get_args(func: Callable[..., Any]) -> List[str]:
-    return list(inspect.signature(func).parameters.keys())
-
-
 @pytest.mark.parametrize("x, y, expected", ((3, 4, 5), (6, 8, 10)))
 def test_pythagorean(x: float, y: float, expected: float):
+    """Test the pythagorean function
+
+    Args:
+        x (float): a float
+        y (float): another float
+        expected (float): expected result of pythagorean(x, y)
+    """
     assert pythagorean(x, y) == expected
